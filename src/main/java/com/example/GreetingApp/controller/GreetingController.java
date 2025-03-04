1e7b9581edff7defd4c2317482ed6c84a04ed20e
Priyanshu7129 <priyanshu7129@gmail.com>
2025-03-04 16:14:34 +0530

    Implemented UC2: Greeting with query parameter

--- src/main/java/com/example/GreetingApp/controller/GreetingController.java
+++ src/main/java/com/example/GreetingApp/controller/GreetingController.java
@@ -1,15 +1,19 @@
 package com.example.GreetingApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class GreetingController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/hello/query")
+    public String sayHelloWithName(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
